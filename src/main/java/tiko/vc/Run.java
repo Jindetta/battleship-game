08483ab3e9e04974e66494367f3696292170096d
Joni Alanko <joni.alanko@cs.tamk.fi>
2018-09-22 23:46:27 +0300

    Method main(): Add Javadoc.
    Added description for method and parameters.

--- src/main/java/tiko/vc/Run.java
+++ src/main/java/tiko/vc/Run.java
@@ -6,7 +6,9 @@ import battleship.Game;
 public class Run {
 
     /**
+     * Starts the program.
      *
+     * @param args Command line arguments. Not used.
      */
     public static void main(String[] args) {
         
