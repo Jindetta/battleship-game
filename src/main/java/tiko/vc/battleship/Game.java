a2fc954bfbbf228dd77cfd22f564d1273a6c18ba
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-09-24 21:55:56 +0300

    Class Game: Define ROWS constant.

--- src/main/java/tiko/vc/battleship/Game.java
+++ src/main/java/tiko/vc/battleship/Game.java
@@ -6,6 +6,11 @@ package battleship;
 public class Game {
 
     /**
+     *
+     */
+    public static final int ROWS = 10;
+
+    /**
      * Stores turn index.
      */
     private int turnIndex;
