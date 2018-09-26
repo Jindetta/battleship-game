d799dbbf61e051b4967349553d692a5acc1422bc
Teemu <teemu.salminen96@gmail.com>
2018-09-26 22:48:38 +0300

    Class TargetingLogic: Add Javadoc for attribute priorityMap.

--- src/main/java/tiko/vc/battleship/TargetingLogic.java
+++ src/main/java/tiko/vc/battleship/TargetingLogic.java
@@ -6,7 +6,7 @@ package battleship;
 public class TargetingLogic {
 	
 	/**
-	 * 
+	 * Contains the information of the game map positions, and priorites the positions.
 	 */
 	private int[] priorityMap;
 
