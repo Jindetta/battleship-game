a41fa71f9681415fd4b94fbddbf981b6526d5fc9
lehdonen <36726382+lehdonen@users.noreply.github.com>
2018-09-27 23:36:05 +0300

    Class Helper: Define TOTAL_PADDING constant.

--- src/main/java/tiko/vc/battleship/Helper.java
+++ src/main/java/tiko/vc/battleship/Helper.java
@@ -4,4 +4,9 @@ package battleship;
  * A class containing all necessary formatting for printing information.
  */
 public class Helper {
+
+    /**
+     *
+     */
+    private static final int TOTAL_PADDING;
 }
