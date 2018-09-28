0e0db2e8652d348d63c71fad6caba922b6ca8a89
lehdonen <36726382+lehdonen@users.noreply.github.com>
2018-09-28 13:50:18 +0300

    Class Helper: Define COLUMN_PADDING constant.

--- src/main/java/tiko/vc/battleship/Helper.java
+++ src/main/java/tiko/vc/battleship/Helper.java
@@ -14,4 +14,9 @@ public class Helper {
      * Define length of spacing between players.
      */
     private static final int PLAYER_SPACING;
+
+    /**
+     *
+     */
+    private static final int COLUMN_PADDING;
 }
