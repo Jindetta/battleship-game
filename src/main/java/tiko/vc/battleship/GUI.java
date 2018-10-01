fb88e0d4f880662ad5fdc05b1d0d97a948fce69e
Teemu <teemu.salminen96@gmail.com>
2018-10-02 01:40:49 +0300

    Class GUI: Add variable WINDOW_TITLE constant.

--- src/main/java/tiko/vc/battleship/GUI.java
+++ src/main/java/tiko/vc/battleship/GUI.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 
 public class GUI extends JFrame {
 	
+	private final String WINDOW_TITLE = "Battleship";
+	
 	private final int WINDOW_WIDTH = 800;
 	private final int WINDOW_HEIGHT = 600;
 	
