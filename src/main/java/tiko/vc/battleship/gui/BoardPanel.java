28c142331e11267521b76487f7fbb11bcc158b14
Teemu <teemu.salminen96@gmail.com>
2018-10-04 00:14:20 +0300

    Class BoardPanel: Add variable BOARD_WIDTH constant.

--- src/main/java/tiko/vc/battleship/gui/BoardPanel.java
+++ src/main/java/tiko/vc/battleship/gui/BoardPanel.java
@@ -1,14 +1,20 @@
 package tiko.vc.battleship.gui;
 
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
+	
+	public static final int BOARD_WIDTH = GUI.WIDTH / 2 - 50;
 	
 	/**
 	 * 
 	 */
 	public BoardPanel() {
+		super(new GridLayout());
 		
+		setSize(5,5);
 	}
 	
 }
