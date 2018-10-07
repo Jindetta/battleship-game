ec54725d6dbca9ce970a0b9c1d0f212749202e4c
Joni Alanko <joni.alanko@cs.tamk.fi>
2018-10-07 17:55:52 +0300

    Class HitLogic: Add currentLogic attribute.

--- src/main/java/tiko/vc/battleship/TargetingLogic.java
+++ src/main/java/tiko/vc/battleship/TargetingLogic.java
@@ -94,5 +94,10 @@ public class TargetingLogic {
      */
     private class HitLogic {
 
+        /**
+         * Define current logic to process.
+         */
+        private int currentLogic;
+
     }
 }
