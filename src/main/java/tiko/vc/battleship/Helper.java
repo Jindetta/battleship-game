7cf7b8b280332385d1ff2171433a3567d76bb566
lehdonen <36726382+lehdonen@users.noreply.github.com>
2018-10-10 01:03:07 +0300

    Method Helper.formatString(): Add JavaDoc.

--- src/main/java/tiko/vc/battleship/Helper.java
+++ src/main/java/tiko/vc/battleship/Helper.java
@@ -56,7 +56,12 @@ public class Helper {
     public static final String COLUMN_STRING;
 
     /**
+     * Formats a string with custom spacing.
      *
+     * @param formatType Format type
+     * @param arg Argument value
+     *
+     * @return formatted string
      */
     public static String formatString(int formatType, Object arg) {
         switch (formatType) {
