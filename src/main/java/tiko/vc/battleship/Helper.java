1218080c585c33c68ca53321e5b4f1e35f28139c
lehdonen <36726382+lehdonen@users.noreply.github.com>
2018-10-10 01:07:11 +0300

    Method Helper.print(): Add method body.

--- src/main/java/tiko/vc/battleship/Helper.java
+++ src/main/java/tiko/vc/battleship/Helper.java
@@ -90,6 +90,12 @@ public class Helper {
     }
 
     /**
+     *
+     */
+    public static void print() {
+    }
+
+    /**
      * Define temporary static initializer to compile.
      */
     static {
