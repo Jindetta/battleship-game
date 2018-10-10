0324a58bf65351294500453b8941ef4f6def9236
Teemu <teemu.salminen96@gmail.com>
2018-10-10 21:36:06 +0300

    Class GameButton: Add getteru & setteru.

--- src/main/java/tiko/vc/battleship/gui/GameButton.java
+++ src/main/java/tiko/vc/battleship/gui/GameButton.java
@@ -7,6 +7,17 @@ public class GameButton extends JButton {
 	private int xPosInBoard;
 	private int yPosInBoard;
 	
-	
+	public int getxPosInBoard() {
+		return xPosInBoard;
+	}
+	public void setxPosInBoard(int xPosInBoard) {
+		this.xPosInBoard = xPosInBoard;
+	}
+	public int getyPosInBoard() {
+		return yPosInBoard;
+	}
+	public void setyPosInBoard(int yPosInBoard) {
+		this.yPosInBoard = yPosInBoard;
+	}	
 
 }
