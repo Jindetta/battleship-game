51db5b4e25b84328b8c18323de8eea8104d55404
Teemu <teemu.salminen96@gmail.com>
2018-10-11 00:15:50 +0300

    Class GameButton: Add Javadoc.

--- src/main/java/tiko/vc/battleship/gui/GameButton.java
+++ src/main/java/tiko/vc/battleship/gui/GameButton.java
@@ -2,16 +2,36 @@ package tiko.vc.battleship.gui;
 
 import javax.swing.JButton;
 
+/**
+ * This class is used to create a single button to the
+ * BoardPanel.
+ * 
+ * @author Teemu
+ *
+ */
 public class GameButton extends JButton {
 	
+	/**
+	 * X position of the button in the 2D array found in BoardPanel.
+	 */
 	private int xPosInBoard;
+	
+	/**
+	 * Y position of the button in the 2D array found in BoardPanel.
+	 */
 	private int yPosInBoard;
 	
+	/**
+	 * 
+	 */
 	public GameButton(String btnText, int xPosInBoard, int yPosInBoard) {
 		super(btnText);
 		setxPosInBoard(xPosInBoard);
 		setyPosInBoard(yPosInBoard);
 	}
+	
+	
+	/* GETTERU & SETTERU */
 	
 	public int getxPosInBoard() {
 		return xPosInBoard;
