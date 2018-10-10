a11a8a68016b9d68b9794c4e0a5f45aef6d4629b
Joni Alanko <joni.alanko@cs.tamk.fi>
2018-10-10 20:10:32 +0300

    Class Directions: Add Javadoc to DIRECTION_N.

--- src/main/java/tiko/vc/battleship/Directions.java
+++ src/main/java/tiko/vc/battleship/Directions.java
@@ -4,5 +4,20 @@ package tiko.vc.battleship;
  * Directions that can be used.
  */
 public enum Directions {
-    DIRECTION_N, DIRECTION_E, DIRECTION_W, DIRECTION_S
+    /**
+     * North direction
+     */
+    DIRECTION_N,
+    /**
+     *
+     */
+    DIRECTION_E,
+    /**
+     *
+     */
+    DIRECTION_W,
+    /**
+     *
+     */
+    DIRECTION_S
 }
